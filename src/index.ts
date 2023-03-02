597f6e0bf3c9b72866a34c9300d0ceb78fed2f1a
frankleo <154284601@qq.com>
2023-03-02 14:11:18 +0800

    temp:临时节点标记 星期四 下午 2:11:18 三月 2日 2023 修改了 package.json 修改了 src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,43 @@
 import moment from "moment";
-import mysql2, { Pool } from "mysql2/promise";
+import mysql2, { Pool } from "mysql2";
 
 let connection: Pool;
 
-process.on("exit", (error) => {
-  console.log("exit==>", error);
-});
-
-process.on("error", (error) => {
-  console.log("error==>", error);
-});
+async function dosome_things() {
+  connection.getConnection(async (error: any, connect) => {
+    if (error) {
+      console.log("获取连接时出错,2s后重新连接... ...", error);
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      dosome_things();
+      return false;
+    };
+    try {
+      console.log(moment().format("YYYY-MM-DD HH:mm:ss"), "获取到连接");
+      setTimeout(() => {
+        connect.release();
+        console.log("连接已释放!");
+      }, 2000);
+    } catch (error) {
+      console.log("业务中出错===>", error);
+      await dosome_things();
+    } finally {
+      connect.release();
+    };
+  });
+};
 
 async function createConnection() {
-  try {
-    connection = await mysql2.createPool({
-      host: "0.0.0.0",
-      port: 43306,
-      user: "root",
-      connectionLimit: 0,
-      password: "glyz205070410"
-    });
+  connection = mysql2.createPool({
+    host: "0.0.0.0",
+    port: 43306,
+    user: "root",
+    connectionLimit: 0,
+    password: "glyz205070410"
+  });
 
-    console.log("连接成功!");
+  console.log("连接成功!");
 
-    setInterval(async () => {
-      try {
-        const connect = await connection.getConnection();
-        console.log(moment().format("YYYY-MM-DD HH:mm:ss"), "获取到连接");
-      } catch (error) {
-        console.log(error);
-      };
-      // connect.release();
-      // console.log("连接已释放");
-    }, 1000);
+  await dosome_things();
 
-  } catch (error) {
-    console.log("连接失败,2s后尝试重新连接...", error);
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    await createConnection();
-    return false;
-  };
 }; createConnection();
 

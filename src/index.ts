60b1cb2a88d004b83134724f04f26f20d1e6ff93
frankleo <154284601@qq.com>
2023-03-02 13:27:05 +0800

    temp:临时节点标记 星期四 下午 1:27:05 三月 2日 2023 修改了 package.json 修改了 src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,37 @@
+import moment from "moment";
 import mysql2, { Pool } from "mysql2/promise";
 
 let connection: Pool;
 
+process.on("exit", (error) => {
+  console.log("exit==>", error);
+});
+
+process.on("error", (error) => {
+  console.log("error==>", error);
+});
+
 async function createConnection() {
-  if (connection) {
-    connection.end();
-    // connection.destroy();
-    // connection.removeAllListeners(["error"]);
-    // connection = null;
-  };
   try {
     connection = await mysql2.createPool({
       host: "0.0.0.0",
       port: 43306,
       user: "root",
-      password: 'glyz205070410'
+      connectionLimit: 0,
+      password: "glyz205070410"
     });
+
     console.log("连接成功!");
 
     setInterval(async () => {
-      const connect = await connection.getConnection();
-      console.log("获取到连接");
-      connect.release();
-      console.log("连接已释放");
+      try {
+        const connect = await connection.getConnection();
+        console.log(moment().format("YYYY-MM-DD HH:mm:ss"), "获取到连接");
+      } catch (error) {
+        console.log(error);
+      };
+      // connect.release();
+      // console.log("连接已释放");
     }, 1000);
 
   } catch (error) {
